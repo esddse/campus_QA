9650300c0ab7cf07c89d89d78aa2cba6a3f739d6
esddse <tjz427@sina.cn>
2017-08-28 13:40:22 +0800

    web update

--- QAsystem.py
+++ QAsystem.py
@@ -4,14 +4,18 @@ import sys
 sys.path.append("./questionClassifier")
 sys.path.append("./qaMatcher")
 
+import time
 from util import *
+import numpy as np
+from optparse import OptionParser
+import pymysql
+
 from sentence_classification_test import *
 from match_test import *
 import sentence_classification_model as SCM
 import match_model as MM
 
-import numpy as np
-from optparse import OptionParser
+
 
 class QuestionAnsweringSystem(object):
 	
@@ -28,7 +32,8 @@ class QuestionAnsweringSystem(object):
 		self.searcher_property = self.ix_property.searcher()
 
 	# 回答一个问题
-	def answer_question(self, question):
+	def answer_question(self, question, is_server=False):
+		print(question)
 		question_type = self.classifier.classify_str(question)
 
 		# find target properties
@@ -57,7 +62,7 @@ class QuestionAnsweringSystem(object):
 							candidate_answers.append([answer_string, answer])
 
 		if len(candidate_answers) == 0:
-			return None
+			return 'No answer'
 
 		questions = [question] * len(candidate_answers)
 		print([candidate_answer[0] for candidate_answer in candidate_answers])
@@ -68,7 +73,18 @@ class QuestionAnsweringSystem(object):
 		for result in sorted_results:
 			print(result[0],' ',result[1])
 
-		return sorted_results[0][0]
+		if is_server == True:
+			answers_text = ''
+			if sorted_results is None:
+				answers_text = 'No answer'
+			else:
+				if len(sorted_results) > 10:
+					sorted_results = sorted_results[:10]
+				for result in sorted_results:
+					answers_text += (str(result[0]) + '\t' + str(result[1]) + '\n\n')
+			return answers_text
+		else:
+			return sorted_results[0][0]
 
 
 
@@ -91,30 +107,65 @@ class QuestionAnsweringSystem(object):
 def main():
 	# define cmd line parameter
 	parser = OptionParser()
+	parser.add_option("-s", "--server",
+		              action="store_true",
+		              dest="is_server",
+		              help="be server")
 	parser.add_option("-f", "--file",
 		              action="store",
 		              type="string",
 		              dest="filepath",
 		              help="path of input file, the file should contain questions only")
+	
 	(options, args) = parser.parse_args()
 
 	# qa system
 	question_answering_system = QuestionAnsweringSystem()
 
-	# read questions from file or from cmd line
-	if options.filepath is None:
+	# if system work for webapp, read question from database
+	if options.is_server == True:
+		db = pymysql.connect('localhost', 'root', 'webkdd', 'esddse', charset='utf8')
+		cursor = db.cursor()
+
 		while True:
-			print ('please type in you question (in Chinese), "exit" or "quit" to exit this program:')
-			question = input()
-			if question == 'exit' or question == 'quit':
-				break
-			elif question == '':
-				continue
-			question_answering_system.answer_questions([question])
+			try:
+				cursor.execute("SELECT * FROM campusQA WHERE is_processed=0 LIMIT 1000")
+				results = cursor.fetchall()
+				for row in results:
+					question = row[0]
+					answers  = question_answering_system.answer_question(question, is_server=True)
+					print(answers)
+					print(question)
+					answers.replace('"', r'\"')
+					answers.replace("'", r"\'")
+					question.replace('"', r'\"')
+					question.replace("'", r"\'")
+					print("UPDATE campusQA SET answer='%s', is_precessed=1, WHERE question LIKE '%s'" % (answers, question))
+					cursor.execute("UPDATE campusQA SET answer='%s', is_processed=1 WHERE question LIKE '%s'" % (answers, question))
+				db.commit()
+			except Exception as e:
+				db.rollback()
+				print(e)
+			time.sleep(0.1)
+
+		cursor.close()
+		db.close()
+
 	else:
-		with open(options.filepath, 'r', encoding='utf8') as f:		
-			questions = [line.strip() for line in f]
-			question_answering_system.answer_questions(questions, write_to_file=True)
+		# read questions from file or from cmd line
+		if options.filepath is None:
+			while True:
+				print ('please type in you question (in Chinese), "exit" or "quit" to exit this program:')
+				question = input()
+				if question == 'exit' or question == 'quit':
+					break
+				elif question == '':
+					continue
+				question_answering_system.answer_questions([question])
+		else:
+			with open(options.filepath, 'r', encoding='utf8') as f:		
+				questions = [line.strip() for line in f]
+				question_answering_system.answer_questions(questions, write_to_file=True)
 
 
 if __name__ == '__main__':
